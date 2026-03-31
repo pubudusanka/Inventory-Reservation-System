88bb38ffac80fc7841eee8da2180e5eacafa657d
Pubudu Sanka <pubudusanka79@gmail.com>
2026-03-31 22:18:33 +0530

    dto created

---
+++ inventory-reservation/src/main/java/com/order/management/inventoryreservation/dto/OrderDto.java
@@ -0,0 +1,16 @@
+package com.order.management.inventoryreservation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderDto {
+
+    private String productId;
+    private int quantity;
+}
