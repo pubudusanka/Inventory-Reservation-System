d2e7f58bfab207824604190c8ae5b76f306df926
Pubudu Sanka <pubudusanka79@gmail.com>
2026-03-31 22:21:31 +0530

    custom exception created

---
+++ inventory-reservation/src/main/java/com/order/management/inventoryreservation/exception/EntryNotFoundException.java
@@ -0,0 +1,12 @@
+package com.order.management.inventoryreservation.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EntryNotFoundException extends RuntimeException{
+
+    public EntryNotFoundException(String message) {
+        super(message);
+    }
+}
