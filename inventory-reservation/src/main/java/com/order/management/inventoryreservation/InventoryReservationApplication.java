94d2cbe9e5792e4d2d2b0b25dbaa9dbc49ffa4aa
Pubudu Sanka <pubudusanka79@gmail.com>
2026-03-31 20:50:00 +0530

    spring boot project import

---
+++ inventory-reservation/src/main/java/com/order/management/inventoryreservation/InventoryReservationApplication.java
@@ -0,0 +1,13 @@
+package com.order.management.inventoryreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryReservationApplication.class, args);
+    }
+
+}
---
+++ inventory-reservation/src/test/java/com/order/management/inventoryreservation/InventoryReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.management.inventoryreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
