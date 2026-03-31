6e5da5e4ec3d995877f327731e0e75a657312c44
Pubudu Sanka <pubudusanka79@gmail.com>
2026-04-01 00:26:46 +0530

    api created

---
+++ inventory-reservation/src/main/java/com/order/management/inventoryreservation/controller/InventoryController.java
@@ -0,0 +1,43 @@
+package com.order.management.inventoryreservation.controller;
+
+import com.order.management.inventoryreservation.dto.OrderDto;
+import com.order.management.inventoryreservation.dto.ProductDto;
+import com.order.management.inventoryreservation.service.InventoryService;
+import com.order.management.inventoryreservation.util.StandardResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @PostMapping("/products")
+    public ResponseEntity<StandardResponseDto> createProduct(@RequestBody ProductDto data){
+        inventoryService.saveProduct(data);
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                    201, "Product Create Successfully!", null
+                ),
+                HttpStatus.CREATED
+        );
+    }
+
+    @PostMapping("/reserve")
+    public ResponseEntity<StandardResponseDto> reserveProduct(@RequestBody OrderDto data){
+        inventoryService.reserveProduct(data);
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                        201, "Order Placed Successfully!", null
+                ),
+                HttpStatus.CREATED
+        );
+    }
+}
